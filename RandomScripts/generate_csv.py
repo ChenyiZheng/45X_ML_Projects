cd453690e3f01566ff445c26db4687aabe9828a7
chenyiz_cpen <chenyizheng97@gmail.com>
2021-01-17 17:16:36 -0800

    Updated generate_csv.py  - changed directory

--- RandomScripts/generate_csv.py
+++ RandomScripts/generate_csv.py
@@ -1,8 +1,8 @@
 import csv
 import os
 
-csv_path = r'E:\cheny\PycharmProjects\local_test\Customized_dataset'
-image_path = r'E:\cheny\PycharmProjects\local_test\Customized_dataset\fire_dataset_complete'
+csv_path = r'E:\cheny\PycharmProjects\45X_ML_Projects\Customized_dataset'
+image_path = r'E:\cheny\PycharmProjects\45X_ML_Projects\Customized_dataset\fire_dataset'
 os.chdir(image_path)
 
 flag = 'non'
